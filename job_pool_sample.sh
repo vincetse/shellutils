ebf2974c3bce361826a7ece750435391c7d8e943
Fruchix <fruchix@gmail.com>
2024-10-29 23:27:13 +0100

    feat: add example of function injection

--- job_pool_sample.sh
+++ job_pool_sample.sh
@@ -33,3 +33,49 @@ job_pool_shutdown
 
 # check the $job_pool_nerrors for the number of jobs that exited non-zero
 echo "job_pool_nerrors: ${job_pool_nerrors}"
+
+
+#####################################################
+# Demonstration of function injection into each job #
+#####################################################
+
+echo -e "\nDemonstration of function injection into each job:"
+
+# sleep some time ($1) then echo something ($2)
+function sleep_n_echo()
+{
+    sleep "$1"
+    echo "$2"
+}
+
+# kill all workers if the local variable "result" from _job_pool_worker
+# indicates that the job failed
+function kill_jobs()
+{
+    # result is undefined in this script, but will be defined when
+    # the function is injected in _job_pool_worker
+    if [[ "${result}" != "0" ]]; then
+        # get the pids of all workers:
+        # - each worker's process is named after the current script (here, job_pool_sample.sh),
+        #   so we use this name to get the pids
+        # - we do not include the current script's pid ($$) as it is not a worker,
+        #   (we do not want to kill the script itself, only the workers)
+        local workers_pids=("$(pgrep -f "$0" | grep -v $$)")
+        kill ${workers_pids[@]} &> /dev/null &
+    fi
+}
+
+# allow 3 parallel jobs, and kill all jobs at the first fail using "kill_jobs" function
+job_pool_init 3 1 kill_jobs
+
+# simulate 3 jobs, where one fails before the others are finished, and interrupts the others
+job_pool_run sleep_n_echo 3 a   # job 1
+job_pool_run /bin/false         # job 2
+job_pool_run sleep_n_echo 3 b   # job 3
+
+# the job 2 will kill all other running workers, using the function "kill_jobs", 
+# that is ran after processing each job
+
+job_pool_shutdown
+
+echo -e "\nOnly the failed job exited, the others did not because they were canceled."
